ebc0d21115859da162379e6b432d17e8ce774520
irtisam ali <irtisam.ali2@gmail.com>
2021-08-11 13:19:03 +0500

    day 5 changes

---
+++ lib/widgets/Homepageitems.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePageItems extends StatelessWidget {
+  final items;
+  const HomePageItems({Key? key, this.items}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Image.network(items.image),
+        title: Text(items.name),
+        subtitle: Text(items.desc),
+        trailing: Text("\$${items.price}"),
+      ),
+    );
+  }
+}
